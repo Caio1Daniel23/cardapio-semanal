d7f61997e476f27c1e4dd32fd20c33d9c6399c9d
Caio Daniel <caiodniel123@gmail.com>
2026-03-16 21:23:28 -0300

    feat: adiciona sistema de cadastro de usuários usando JSON

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div>
+        <h1>Acesso Exclusivo ao Cardápio</h1>
+        <h2>Login do Restaurante</h2>
+    </div>
+
+    <form action="">
+        <label for="usuario">Usuário</label>
+        <input type="text" id="usuario" name="usuario">
+
+        <label for="senha">Senha</label>
+        <input type="password" id="senha" name="senha">
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+
+</html>
